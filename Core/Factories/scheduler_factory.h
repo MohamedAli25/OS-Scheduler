139adfed1d1830e327e61b7b167b20126b6aa648
Mohamed Gamal <mgtm.prog@gmail.com>
2020-03-31 00:49:50 +0200

    Added SupportedSchedulers

--- Core/Factories/scheduler_factory.h
+++ Core/Factories/scheduler_factory.h
@@ -1,28 +1,20 @@
 #ifndef SCHEDULER_FACTORY_H_
 #define SCHEDULER_FACTORY_H_
 
+#include <QStringList>
+#include <QString>
 #include "../Schedulers/scheduler.h"
 #include "../Schedulers/f_c_f_s.h"
-#include "../Schedulers/priority_nonpreemptive.h"
-#include "../Schedulers/priority_preemptive.h"
-#include "../Schedulers/round_robin.h"
-#include "../Schedulers/s_j_f_nonpreemptive.h"
-#include "../Schedulers/s_j_f_preemptive.h"
+//#include "../Schedulers/priority_nonpreemptive.h"
+//#include "../Schedulers/priority_preemptive.h"
+//#include "../Schedulers/round_robin.h"
+//#include "../Schedulers/s_j_f_nonpreemptive.h"
+//#include "../Schedulers/s_j_f_preemptive.h"
 
-typedef enum SchedulerEnum
-{
-    f_c_f_s,
-    priority_nonpreemptive,
-    priority_preemptive,
-    round_robin,
-    s_j_f_nonpreemptive,
-    s_j_f_preemptive
-} SchedulerEnum;
-
-class SchedulerFactory
-{
+class SchedulerFactory{
 public:
-    static Scheduler *schedulerFactoryMethod(SchedulerEnum scheduler, unsigned long long quantum = 0);
+    static Scheduler *createScheduler(QString scheduler, unsigned long long quantum = 0);
+    static QStringList SupportedSchedulers;
 };
 
 #endif /* SCHEDULER_FACTORY_H_ */
