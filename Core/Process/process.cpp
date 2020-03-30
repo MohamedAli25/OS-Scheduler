a41fee932532d5a291f379e86e156aaeea636054
Mohamed Gamal <mgtm.prog@gmail.com>
2020-03-30 21:19:59 +0200

    Used QString class instead of std::string

--- Core/Process/process.cpp
+++ Core/Process/process.cpp
@@ -1,35 +1,29 @@
 #include "process.h"
 
-Process::Process(string name, unsigned long long burstTime, unsigned long long arrivalTime) : name{name}, burstTime{burstTime}, arrivalTime{arrivalTime}
-{
-}
+Process::Process(QString name, unsigned long long burstTime, unsigned long long arrivalTime):
+    name{name}, burstTime{burstTime}, arrivalTime{arrivalTime}
+{}
 
-void Process::setName(string name)
-{
+void Process::setName(QString name){
     this->name = name;
 }
 
-string Process::getName()
-{
+QString Process::getName(){
     return name;
 }
 
-void Process::setBurstTime(unsigned long long burstTime)
-{
+void Process::setBurstTime(unsigned long long burstTime){
     this->burstTime = burstTime;
 }
 
-unsigned long long Process::getBurstTime()
-{
+unsigned long long Process::getBurstTime(){
     return burstTime;
 }
 
-void Process::setArrivalTime(unsigned long long arrivalTime)
-{
+void Process::setArrivalTime(unsigned long long arrivalTime){
     this->arrivalTime = arrivalTime;
 }
 
-unsigned long long Process::getArrivalTime()
-{
+unsigned long long Process::getArrivalTime(){
     return this->arrivalTime;
 }
