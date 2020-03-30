bd835a16ec675e7deb2335eec8440ec743a668b0
Mohamed Gamal <mgtm.prog@gmail.com>
2020-03-31 01:00:39 +0200

    Add Process takes pointer to a process not process by value

--- Core/Schedulers/f_c_f_s.cpp
+++ Core/Schedulers/f_c_f_s.cpp
@@ -1,26 +1,19 @@
 #include "f_c_f_s.h"
 
-bool FCFS::compare(Process p1, Process p2)
-{
+bool FCFS::compare(Process p1, Process p2){
     return (p1.getArrivalTime() < p2.getArrivalTime());
 }
 
-void FCFS::addProcess(Process process)
-{
-    processes.push(process);
-    arrivalTimeSum += process.getArrivalTime();
-    burstTimeSum += process.getBurstTime();
+void FCFS::addProcess(Process *process){
+    processes.push(*process);
+    arrivalTimeSum += process->getArrivalTime();
+    burstTimeSum += process->getBurstTime();
     numberOfProcesses += 1;
 }
 
-Process *FCFS::next(double currentTime, double timeSlice)
-{
-    if (processes.front().getRemainingBurstTime() <= 0)
-    {
-        finishTimeSum += currentTime;
-        processes.pop();
-    }
-    Process *currentProcess = &processes.front();
-    currentProcess->setRemainingBurstTime(currentProcess->getRemainingBurstTime() - timeSlice);
-    return currentProcess;
+Process *FCFS::next(double currentTime, double timeSlice){
+//    if (processes.front().getRemainingBurstTime() <= 0){
+//        finishTimeSum += currentTime;
+//        processes.pop();
+//    }
 }
