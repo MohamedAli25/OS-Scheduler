0faf78c8a111d182d0c72413bed4636dcd9cf428
Mohamed Gamal <mgtm.prog@gmail.com>
2020-03-31 00:34:20 +0200

    changes brackets style

--- Core/Schedulers/scheduler.cpp
+++ Core/Schedulers/scheduler.cpp
@@ -1,26 +1,21 @@
 #include "scheduler.h"
 
-unsigned long long Scheduler::getArrivalTimeSum()
-{
+unsigned long long Scheduler::getArrivalTimeSum(){
     return arrivalTimeSum;
 }
 
-unsigned long long Scheduler::getBurstTimeSum()
-{
+unsigned long long Scheduler::getBurstTimeSum(){
     return burstTimeSum;
 }
 
-unsigned long long Scheduler::getFinishTimeSum()
-{
+unsigned long long Scheduler::getFinishTimeSum(){
     return finishTimeSum;
 }
 
-unsigned long long Scheduler::getNumberOfProcesses()
-{
+unsigned long long Scheduler::getNumberOfProcesses(){
     return numberOfProcesses;
 }
 
-double Scheduler::getAverageWaitingTime()
-{
+double Scheduler::getAverageWaitingTime(){
     return (static_cast<double>(finishTimeSum - arrivalTimeSum - burstTimeSum) / numberOfProcesses);
 }
