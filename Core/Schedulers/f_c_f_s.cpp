5eafd8ea1ce6060343fee7f59f6ea2d1a5819cee
Mohamed Gamal <mgtm.prog@gmail.com>
2020-03-30 21:17:56 +0200

    Changed brackets style to save space on screen

--- Core/Schedulers/f_c_f_s.cpp
+++ Core/Schedulers/f_c_f_s.cpp
@@ -1,36 +1,29 @@
 #include "f_c_f_s.h"
 #include <queue>
 
-bool FCFS::compare(Process p1, Process p2)
-{
+bool FCFS::compare(Process p1, Process p2){
     return (p1.getArrivalTime() < p2.getArrivalTime());
 }
 
-void FCFS::addProcess(Process process)
-{
+void FCFS::addProcess(Process process){
     processes.push_back(process);
 }
 
-vector<pair<string, unsigned long long>> FCFS::generateTimeline()
-{
+vector<pair<string, unsigned long long>> FCFS::generateTimeline(){
     vector<pair<string, unsigned long long>> timeline;
     sort(processes.begin(), processes.end(), compare);
     unsigned long long time = 0;
-    for (Process p : processes)
-    {
+    for (Process p : processes)    {
         this->arrivalTimeSum += p.getArrivalTime();
         this->burstTimeSum += p.getBurstTime();
         this->numberOfProcesses++;
-        if (time < p.getArrivalTime())
-        {
+        if (time < p.getArrivalTime())        {
             pair<string, unsigned long long> emptyInterval = {"None", p.getArrivalTime() - time};
             timeline.push_back(emptyInterval);
             pair<string, unsigned long long> busyInterval = {p.getName(), p.getBurstTime()};
             timeline.push_back(busyInterval);
             time = p.getArrivalTime() + p.getBurstTime();
-        }
-        else
-        {
+        }else{
             pair<string, unsigned long long> busyInterval = {p.getName(), p.getBurstTime()};
             timeline.push_back(busyInterval);
             time += p.getBurstTime();
