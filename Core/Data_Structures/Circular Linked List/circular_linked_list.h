d21001e50a0e906c68f75157122b793cf2ecb542
theMeanScore <m.ad.ali2165@gmail.com>
2020-04-02 00:56:02 +0200

    Finish the header file of circular linked list

--- Core/Data_Structures/Circular Linked List/circular_linked_list.h
+++ Core/Data_Structures/Circular Linked List/circular_linked_list.h
@@ -1,9 +1,21 @@
 #ifndef CIRCULAR_LINKED_LIST_H_
 #define CIRCULAR_LINKED_LIST_H_
 
+#include "c_l_l_node.h"
+
 template <typename T>
 class CircularLinkedList
 {
+private:
+    CLLNode<T> *root = nullptr;
+    unsigned long long linkedListSize = 0;
+
+public:
+    void add(T value);
+    void removePtr(CLLNode<T> *node);
+    bool isEmpty();
+    unsigned long long size();
+    void clear();
 };
 
 #endif /* CIRCULAR_LINKED_LIST_H_ */
