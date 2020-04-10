2f09710da53abac09f4eedf51f81794928094fae
theMeanScore <m.ad.ali2165@gmail.com>
2020-04-10 06:50:51 +0200

    Improve FCFS

--- Core/Schedulers/f_c_f_s.cpp
+++ Core/Schedulers/f_c_f_s.cpp
@@ -1,7 +1,7 @@
 #include "f_c_f_s.h"
 
-
-FCFS::FCFS() {
+FCFS::FCFS()
+{
 }
 
 void FCFS::addProcess(Process *process)
@@ -20,17 +20,17 @@ Process *FCFS::next(double currentTime, double timeSlice)
     {
         if (processes.front().getRemainingBurstTime() <= 0)
         {
-            finishTimeSum += currentTime;
             processes.pop();
         }
         if (!processes.empty())
         {
             process_ptr = &processes.front();
-            if(process_ptr->getRemainingBurstTime() == process_ptr->getBurstTime())
+            if (process_ptr->getRemainingBurstTime() == process_ptr->getBurstTime())
                 process_ptr->setStartTime(currentTime);
             process_ptr->setRemainingBurstTime(process_ptr->getRemainingBurstTime() - timeSlice);
-            if (process_ptr->getRemainingBurstTime() == 0)
+            if (process_ptr->getRemainingBurstTime() <= 0)
             {
+                finishTimeSum += currentTime;
                 process_ptr->setRemainingBurstTime(0);
                 process_ptr->setEndTime(currentTime);
             }
@@ -44,7 +44,7 @@ ProcessEnum FCFS::getProcessType()
     return ProcessEnum::NORMAL;
 }
 
-
-FCFS::~FCFS() {
+FCFS::~FCFS()
+{
     this->Scheduler::~Scheduler();
 }
