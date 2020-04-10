d587c77417a6ca5195c0a2d902f1e76d9656e763
Mohamed Gamal <mgtm.prog@gmail.com>
2020-04-10 07:09:00 +0200

    commented test cases

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 #include "Core/Factories/scheduler_factory.h"
 #include "Core/Factories/process_factory.h"
+#include "Core/Data_Structures/Min_Priority_Queue/min_priority_queue.h"
 
 #include <QApplication>
 
@@ -14,18 +15,19 @@ int main(int argc, char *argv[])
 //    styleFile.open(QIODevice::ReadOnly);
 //    QString style = styleFile.readAll();
 //    a.setStyleSheet(style);
-//    MainWindow w;
-//    w.show();
-
-    Scheduler *s = SchedulerFactory::createScheduler("Round Robin");
-    Process *p1 = ProcessFactory::createProcess(s->getProcessType(),0,"p1",3,0);
-    Process *p2 = ProcessFactory::createProcess(s->getProcessType(),0,"p2",5,0);
-    s->addProcess(p1);
-    s->addProcess(p2);
-    qDebug() << s->next(1,1)->getName();
-    qDebug() << s->next(1,1)->getName();
-    qDebug() << s->next(1,1)->getName();
-    qDebug() << s->next(1,1)->getName();
+    MainWindow w;
+    w.show();
+//    MinPriorityQueue<Process> min(Process::lessArrivalTime);
+//    Scheduler *s = SchedulerFactory::createScheduler("Round Robin");
+//    Process *p1 = ProcessFactory::createProcess(s->getProcessType(),0,"p1",3,0);
+//    Process *p2 = ProcessFactory::createProcess(s->getProcessType(),0,"p2",5,0);
+//    s->addProcess(p1);
+//    s->addProcess(p2);
+//    qDebug() << s->next(1,1)->getName();
+//    qDebug() << s->next(1,1)->getName();
+//    qDebug() << s->next(1,1)->getName();
+//    qDebug() << s->next(1,1)->getName();
     qDebug("Finished");
+
     return a.exec();
 }
