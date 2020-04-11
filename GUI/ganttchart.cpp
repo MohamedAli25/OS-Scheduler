1df7fa4e7a3f4c5ed3967312e49d498047fcc2f2
Mohamed Gamal <mgtm.prog@gmail.com>
2020-04-12 01:38:30 +0200

    Added Beautify Functionality

--- GUI/ganttchart.cpp
+++ GUI/ganttchart.cpp
@@ -1,17 +1,17 @@
 #include "ganttchart.h"
 
 GanttChart::GanttChart(){
+    this->setLayout(new QVBoxLayout);
+
     chart = new QChart;
     series = new QHorizontalStackedBarSeries;
-
-    this->setChart(chart);
+    chartView = new QChartView(chart);
+    this->layout()->addWidget(chartView);
     chart->setTitle("Processes Gantt Chart");
     chart->legend()->setAlignment(Qt::AlignBottom);
     chart->addSeries(series);
-    chart->createDefaultAxes();
     axis = new QBarCategoryAxis();
-    chart->setAxisY(axis, series);
-    chart->axisX()->setRange(0,1);
+
     setMap.insert("GAP",Qt::white);
 }
 
@@ -29,16 +29,83 @@ void GanttChart::addValue(QString setName, int value){
     series->append(set);
     chart->removeSeries(series);
     chart->addSeries(series);
-    chart->createDefaultAxes();
+//    chart->createDefaultAxes();
     for(int a : setToBeRemoved) chart->legend()->markers()[a]->setVisible(false);
     setMap.insert(setName, chart->legend()->markers()[counter]->brush().color());
     counter ++;
+    if(timeLine.size() && !timeLine[timeLine.size()-1].first.compare(setName)){
+        timeLine[timeLine.size()-1].second++;
+    }else{
+        timeLine.append(QPair<QString, int>(setName,1));
+    }
 }
 
 void GanttChart::reset(){
-    series->clear();
+    colorCounter = 0;
     counter = 0;
     setMap.clear();
     setToBeRemoved.clear();
-    setMap.insert("GAP",Qt::white);
+    timeLine.clear();
+    setMap.insert("GAP",Qt::gray);
+    if(!beautified){
+        series->clear();
+    }else{
+        delete gScene;
+        delete gView;
+        chart = new QChart;
+        series = new QHorizontalStackedBarSeries;
+        chartView = new QChartView(chart);
+        this->layout()->addWidget(chartView);
+        chart->setTitle("Processes Gantt Chart");
+        chart->legend()->setAlignment(Qt::AlignBottom);
+        chart->addSeries(series);
+        axis = new QBarCategoryAxis();
+    }
+    beautified = false;
+}
+
+void GanttChart::beautify(){
+    delete chartView;
+    beautified = true;
+    gScene = new QGraphicsScene();
+    gView = new QGraphicsView(gScene);
+    gView->setMinimumWidth(800);
+    this->layout()->addWidget(gView);
+    int x = 0;
+    int screenW = this->size().rwidth() - 20;
+    int ganttWidth = 0;
+    for(auto a : timeLine){
+        ganttWidth += a.second;
+    }
+    qDebug() << ganttWidth;
+    QGraphicsTextItem *s = gScene->addText("0");
+    s->setPos(x+10,80);
+    s->setFont(QFont("",7));
+    for(auto a : timeLine){
+        qDebug() << a.first << a.second << setMap[a.first];
+        QGraphicsRectItem *rect = new QGraphicsRectItem(x+10,40,(screenW/ganttWidth)*a.second,30);
+        rect->setBrush(setMap[a.first]);
+        gScene->addItem(rect);
+        s = gScene->addText(QString::number((x+10)/(10*(screenW/ganttWidth))));
+        s->setPos(x+10,80);
+        s->setFont(QFont("",7));
+        x += (screenW/ganttWidth)*a.second;
+    }
+    s = gScene->addText(QString::number(x/(10*(screenW/ganttWidth))));
+    s->setPos(x+10,80);
+    s->setFont(QFont("",7));
+    s = gScene->addText("Beautified Gantt Chart");
+    s->setPos((screenW/2)-120,0);
+    s->setFont(QFont("",12));
+
+    x = 20;
+    for(auto key : setMap.keys()){
+        QGraphicsRectItem *rect = new QGraphicsRectItem(x+10,120,10,10);
+        rect->setBrush(setMap[key]);
+        gScene->addItem(rect);
+        s = gScene->addText(key);
+        s->setPos(x+20,112);
+        x+= 100;
+    }
+
 }
--- GUI/ganttchart.h
+++ GUI/ganttchart.h
@@ -2,6 +2,7 @@
 #define GANTTCHART_H
 
 #include <QtCharts/QChartView>
+#include <QWidget>
 #include <QChart>
 #include <QHorizontalStackedBarSeries>
 #include <QBarSet>
@@ -9,26 +10,35 @@
 #include <QMap>
 #include <QDebug>
 #include <QBarCategoryAxis>
+#include <QVBoxLayout>
+#include <QGraphicsView>
+#include <QGraphicsScene>
 
 using namespace  QtCharts;
 
-class GanttChart : public QChartView{
+class GanttChart : public QWidget{
     Q_OBJECT
 public:
     GanttChart();
     void addValue(QString setName, int value);
+    void beautify();
     void reset();
 
 private:
+    QChartView *chartView;
     QChart *chart;
     QHorizontalStackedBarSeries *series;
     QMap<QString, QColor> setMap;
     QVector<int> setToBeRemoved;
     QBarCategoryAxis *axis;
+    QGraphicsView *gView;
+    QGraphicsScene *gScene;
     int counter = 0;
     int colorCounter = 0;
     QList<QColor> colors = {QColor(23,126,137), QColor(8,76,97), QColor(219,58,52),
                             QColor(255,200,87), QColor(50,48,49), QColor(0,0,0),};
+    QVector<QPair<QString, int>> timeLine;
+    bool beautified = false;
 
 
 };
